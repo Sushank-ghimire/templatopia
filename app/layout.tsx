fd68485829254ab9f87ec44abb0e55cfc929a7e2
Sushank Ghimire <sushank.815421@bkmc.tu.edu.np>
2025-05-04 15:08:13 +0545

    Add favicon images and web manifest; update layout to include favicon links

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
 import Navbar from "@/components/ui/Navbar";
 import Footer from "@/components/ui/Footer";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +18,6 @@ export const metadata: Metadata = {
       "Find the perfect website template for your next project. High-quality templates and UI components for modern web development.",
     url: "https://templatopia.ghimiresushank.com.np",
     siteName: "Templatopia",
-    images: [
-      {
-        url: "/Logo.png",
-        width: 1200,
-        height: 630,
-        alt: "Templatopia - Premium Website Templates",
-      },
-    ],
     locale: "en_US",
     type: "website",
   },
@@ -43,6 +36,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Favicons */}
+        <link rel="icon" href="/favicon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/android-chrome-192x192.png"
+        />
+        <link rel="icon" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body
         className={`${inter.className} min-h-screen flex flex-col bg-gradient-to-b from-gray-50 to-white dark:from-gray-900 dark:to-gray-800 overflow-x-hidden`}
       >
